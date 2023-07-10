5c228c84b3ed1f06664299e63d1a3d33217d94a3
Lumen <123202753+Lumen1024@users.noreply.github.com>
2023-07-10 20:35:46 +0700

    Commend ru CreateProduct

--- app/src/main/java/com/example/kateapp/createProduct/CreateProductViewModel.kt
+++ app/src/main/java/com/example/kateapp/createProduct/CreateProductViewModel.kt
@@ -7,6 +7,7 @@ import com.example.kateapp.Screen
 import com.example.kateapp.data.Product
 import com.example.kateapp.data.ProductDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -16,15 +17,19 @@ import javax.inject.Inject
 class CreateProductViewModel @Inject constructor(
     private val dao: ProductDao
 ) : ViewModel() {
+    // Идёт ли добавление в базу
     private val _isLoading = MutableStateFlow(false)
     val isLoading = _isLoading.asStateFlow()
 
+    // Имя продукта
     private val _name = MutableStateFlow("")
     val name = _name.asStateFlow()
 
+    // Цена продукта
     private val _price = MutableStateFlow(0)
     val price = _price.asStateFlow()
 
+    // Количество продукта
     private val _count = MutableStateFlow(0)
     val count = _count.asStateFlow()
 
@@ -34,10 +39,14 @@ class CreateProductViewModel @Inject constructor(
 
     fun onCountEdit(count: Int) { _count.value = count }
 
+    // onAdd вызывается при добавлении продукта и запускает в потоке viewModel
+    // добавление в базу данных и устанавливается состояние загрузки.
+    // После этого запускается ожидание добавления в потоке viewModel
+    // и переход на главный экран после того как продукт добавлен в базу
     fun onAdd(navHostController: NavHostController) {
         val addJob = viewModelScope.launch {
             _isLoading.value = true
-            //delay(3000L)
+            delay(300L)
             dao.insertProduct(Product(
                 name = name.value,
                 price = price.value,
